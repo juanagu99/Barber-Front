336fafcfdcb301d07b4a897cfa95b26abd86526c
Juan Pablo Agudelo Florez <juan.ablo1999@gmail.com>
2021-09-06 12:22:23 -0500

    Configuración producciòn

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,6 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  console.log("ambiente de producción");
-  console.log("url:"+environment.url_service);
-}else{
-  console.log("ambiente de desarrollo");
-  console.log("url:"+environment.url_service);
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
